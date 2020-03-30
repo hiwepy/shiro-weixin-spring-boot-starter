d09e84a3e2df872a7435c863ea6276bf769611a5
wandl <hnxyhcwdl1003@163.com>
2020-03-31 00:41:35 +0800

    xg

--- src/main/java/org/apache/shiro/spring/boot/ShiroWeiXinWebAutoConfiguration.java
+++ src/main/java/org/apache/shiro/spring/boot/ShiroWeiXinWebAutoConfiguration.java
@@ -1,5 +1,7 @@
 package org.apache.shiro.spring.boot;
 
+import org.apache.shiro.spring.boot.jwt.JwtPayloadRepository;
+import org.apache.shiro.spring.boot.weixin.authc.WxAuthenticationSuccessHandler;
 import org.apache.shiro.spring.web.config.AbstractShiroWebConfiguration;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -20,7 +23,13 @@ public class ShiroWeiXinWebAutoConfiguration extends AbstractShiroWebConfigurati
 
 	private ApplicationContext applicationContext;
 	
-
+	@Bean
+	protected WxAuthenticationSuccessHandler wxAuthenticationSuccessHandler(
+			JwtPayloadRepository jwtPayloadRepository,
+			ShiroJwtProperties jwtProperties) {
+		return new WxAuthenticationSuccessHandler(jwtPayloadRepository, jwtProperties.isCheckExpiry());
+	}
+ 
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
